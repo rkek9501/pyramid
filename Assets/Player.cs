ad9d610ba8a3bddb76eb1c1ea1ad6f4beda07034
doyoung <arktics@naver.com>
2018-06-07 12:00:18 +0900

    맵생성

---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    //GameObject boor;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "DOOR")
+        {
+            Debug.Log("충돌");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "DOOR")
+        {
+            Debug.Log("충돌");
+        }
+    }
+}
